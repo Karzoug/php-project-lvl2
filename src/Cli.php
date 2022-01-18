29b96a8f02ad5ccfcc70315a0b376fcdf8628e8e
Aleksandr Baranov <a.n.baranov@outlook.com>
2022-01-18 21:27:31 +0300

    feat: add arguments (filenames) and option (format) for utility

--- src/Cli.php
+++ src/Cli.php
@@ -8,12 +8,14 @@ function start()
 Generate diff
 
 Usage:
-  gendiff (-h|--help)
-  gendiff (-v|--version)
-
+    gendiff (-h|--help)
+    gendiff (-v|--version)
+    gendiff [--format <fmt>] <firstFile> <secondFile>
+    
 Options:
-  -h --help                     Show this screen
-  -v --version                  Show version
+    -h --help                     Show this screen
+    -v --version                  Show version
+    --format <fmt>                Report format [default: stylish]
 
 DOC;
 
