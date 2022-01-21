fc475c055eb8957f263a0dfcebcce8b6acdf2c58
Aleksandr Baranov <a.n.baranov@outlook.com>
2022-01-21 14:28:57 +0300

    test: add tests for yaml format files

--- tests/GenTest.php
+++ tests/GenTest.php
@@ -14,18 +14,32 @@ class GenTest extends TestCase
         return realpath(implode('/', $parts));
     }
 
-    public function testMain1(): void
+    public function testMainJSON1(): void
     {
          
         $fileName1 = $this->getFixtureFullPath("file1.json");
-        $fileName2 = $this->getFixtureFullPath("file2.json");
+        $fileName2 = $this->getFixtureFullPath("file2.JSON");
         $this->assertStringEqualsFile($this->getFixtureFullPath("result12.txt"), genDiff($fileName1, $fileName2));
     }
 
-    public function testMain2(): void
+    public function testMainJSON2(): void
     {
         $fileName1 = $this->getFixtureFullPath("file1.json");
         $fileName2 = $this->getFixtureFullPath("blank.json");
+        $this->assertStringEqualsFile($this->getFixtureFullPath("result1blank.txt"), genDiff($fileName1, $fileName2));
+    }
+
+    public function testMainYAML1(): void
+    {
+        $fileName1 = $this->getFixtureFullPath("file1.yml");
+        $fileName2 = $this->getFixtureFullPath("file2.YAML");
+        $this->assertStringEqualsFile($this->getFixtureFullPath("result12.txt"), genDiff($fileName1, $fileName2));
+    }
+
+    public function testMainYAML2(): void
+    {
+        $fileName1 = $this->getFixtureFullPath("file1.yml");
+        $fileName2 = $this->getFixtureFullPath("blank.yaml");
         $this->assertStringEqualsFile($this->getFixtureFullPath("result1blank.txt"), genDiff($fileName1, $fileName2));
     }
 }
