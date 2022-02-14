fa853014155da2f5b006be64f6946f482d8a534f
Aleksandr Baranov <a.n.baranov@outlook.com>
2022-02-14 14:08:06 +0300

    refactor: fix some phpstan analyse errors

--- src/Diff.php
+++ src/Diff.php
@@ -11,71 +11,71 @@ const NESTED = 'nested';
 function getDiff(array $dict1, array $dict2)
 {
     $dictKeys = array_unique(array_merge(array_keys($dict1), array_keys($dict2)));
-    sort($dictKeys);
+    $sortedDictKeys = collect($dictKeys)->sort()->all();
 
-    $result = array_reduce($dictKeys, function ($acc, $key) use ($dict1, $dict2) {
+    $result = array_reduce($sortedDictKeys, function ($acc, $key) use ($dict1, $dict2) {
         if (!array_key_exists($key, $dict1)) {
-            $acc[] = [
+            $acc->push([
                 "key" => $key,
                 "before" => null,
                 "after" => $dict2[$key],
                 "children" => null,
                 "status" => ADDED
-            ];
+            ]);
             return $acc;
         }
         if (!array_key_exists($key, $dict2)) {
-            $acc[] = [
+            $acc->push([
                 "key" => $key,
                 "before" => $dict1[$key],
                 "after" => null,
                 "children" => null,
                 "status" => REMOVED
-            ];
+            ]);
             return $acc;
         }
 
         if (is_array($dict1[$key]) ^ is_array($dict2[$key])) {
-            $acc[] = [
+            $acc->push([
                 "key" => $key,
                 "before" => $dict1[$key],
                 "after" => $dict2[$key],
                 "children" => null,
                 "status" => UPDATED
-            ];
+            ]);
             return $acc;
         }
 
         if (is_array($dict1[$key]) && is_array($dict2[$key])) {
-            $acc[] = [
+            $acc->push([
                 "key" => $key,
                 "before" => $dict1[$key],
                 "after" => $dict2[$key],
                 "children" => getDiff($dict1[$key], $dict2[$key]),
                 "status" => NESTED
-            ];
+            ]);
             return $acc;
         }
 
         if ($dict1[$key] === $dict2[$key]) {
-            $acc[] = [
+            $acc->push([
                 "key" => $key,
                 "before" => $dict1[$key],
                 "after" => $dict2[$key],
                 "children" => null,
                 "status" => UNCHANGED
-            ];
+            ]);
         } else {
-            $acc[] = [
+            $acc->push([
                 "key" => $key,
                 "before" => $dict1[$key],
                 "after" => $dict2[$key],
                 "children" => null,
                 "status" => UPDATED
-            ];
+            ]);
         }
         return $acc;
-    }, []);
+    }, collect([]));
 
-    return $result;
+    return $result->all();
 }
